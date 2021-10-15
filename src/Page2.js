62b3bad04b2c826cae69f16c01064de87d9fe457
Seungil <schoi0115@gmail.com>
2021-10-15 00:18:06 -0400

    overflow css

--- src/Page2.js
+++ src/Page2.js
@@ -14,11 +14,28 @@ function Page2() {
                 </h2>
                 Office Manager 08/2019 - 02/2021
                 <br />
-                ● Created contracts for Teaching Artists
+                •	Creates signed LOA, MOI, and MOU for Teaching Artists, 1099 employees contract
                 <br />
-                ● Assisted other teams' administrative tasks upon request
+                •	Bookkeeping, Monthly Reconcile, Annual Finance Reports, Finance Assistance
                 <br />
-                ● Quickbooks Tasks
+                •	Quickbooks Tasks
+                <br />
+                •	Reconcile monthly and annual bank account and payroll; performed monthly-closing and yearly-closing reports
+                <br />
+                •	Invoicing and issuing checks
+                <br />
+                •	Coordinate with Lifetime Arts’s CPA and provide documents as request
+                <br />
+                •	Assist other teams' administrating tasks upon the request
+                <br />
+                •	Scheduling upcoming training and provide logistical assistance
+                <br />
+                •	Administrating office supplies and office tasks, Excel, PDF editing, Words, Google Slides, Google Docs, Google Sheets, Google drive.
+                <br />
+                •	Stage Manager on Zoom for Teaching Artists training and Organization meetings
+                <br />
+                •	Create meetings and organize via Google Meet and Zoom
+
                 <br />
                 <br />
 
@@ -28,22 +45,32 @@ function Page2() {
          
                 Recruiter 01/2019 - 08/2019
                 <br />
-                ● Interviewed potential candidates and reviewed plans for career growth; provided quick feedback for customer service
-                satisfaction
+                •	Managed requisitions and offer process; maintained high-volume clientele and consulted with hiring managers 
                 <br />
-                ● Managed requisitions and offer process; maintained high-volume clientele and consulted with hiring managers
+                •	Interviewed potential candidates and reviewed plans for career growth; provided quick feedback for customer service satisfaction 
+                <br />
+                •	Participated in quarterly business reviews, diversity plans/execution strategies
+                <br />
+                •	Verified clienteles’ references to ensure accurate information
+                <br />
+                •	Performed screenings and in-person interviews throughout recruiting processes
+
                 <br />
                 <br />
                 <h2>
-                BEST FINE ARTS, INC. Englewood, NJ
+                BEST FINE ARTS, INC.         Englewood, NJ
                 </h2>
                 
                 General Manager 01/2016 - 05/2018
                 <br />
-                ● Developed and maintained business relationships with domestic and international companies (i.e, TJX: T.J. Maxx, HomeGoods and HomeSense)
+                •	Developed and maintained business relationships with domestic and international companies (i.e, TJX: T.J. Maxx, HomeGoods and HomeSense)
                 <br />
-                ● Analyzed monthly and annual sales; performed monthly and yearly closing reports using Quickbooks
+                •	Analyzed monthly and annual sales; performed monthly and yearly closing reports using Quickbooks
                 <br />
+                •	Allocated and sold products during East and West Coast Art Shows <br />(i.e., Las Vegas Art Expo 2018 in Las Vegas, West Coast Art & Frame 2017 & 2018 in Las Vegas, New York Art Expo 2016 & 2017 in NYC)
+                <br />
+                •	Drafted, submitted and tracked all PO; resolved PO requisitions and created invoices 
+
                
             </p>
         </div>
