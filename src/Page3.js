454da47e8ede53f94e0c2e8e4ac50c71b6402f2e
Seungil <schoi0115@gmail.com>
2021-10-14 22:02:37 -0400

    add application and update resume

--- src/Page3.js
+++ src/Page3.js
@@ -7,41 +7,144 @@ function Page3() {
 
 
     return (
-        <div className="typearea">
-     
-            <h1>Applications</h1>
-            <a href= "https://github.com/schoi0115" className="appgithub">{"Shawn's Git Hub"} </a>
-           
-            <br />
-            <br />
-          
+        <div className="typearea_application">
+                <h1>Applications</h1>
+                <p>
+                <a href= "https://github.com/schoi0115" className="appgithub">{"Shawn's Git Hub"} </a>
+            
+                <br />
+                <br />
+                 
+                <a href= "https://github.com/schoi0115/medipill">{"Medi Pill"} </a>
+                    <li>
+                    Use Ruby on rails as a Backend and React for Frontend
+                    </li>
+                    <li>
+                    Using bycript and localStorage to store user information. 
+                    </li>
+                    <li>
+                    Using serializer creating validations
+                    </li>
+                    <li>
+                    Be able to add your medicine frequency system
+                    </li>
+                    <li>
+                    Be able to find local hospitals and pharmacies including your location
+                    </li>
+                    <li>
+                    Connect your current dose to the gauge to manage your dose level
+                    </li>
+                    <li>
+                    RESTful API request, /GET, /POST, /DELETE, /PATCH
+                    </li>
+                    <li>
+                    GoogleMap search feature and Unique styling
+                    </li>
 
-                    
-                    <a href= "https://github.com/schoi0115/game_app">{"Game-app"} </a>
+                        <br />
+                <a href= "https://github.com/schoi0115/project-n">{"Nurse Tracker application"} </a>
+                    <li>
+                    RESTful API request, /GET, /POST, /DELETE, /PATCH
+                    </li>
+                    <li>
+                    Using bycript and localStorage to store user information. 
+                    </li>
+                    <li>
+                    Using serializer creating validations
+                    </li>
+                    <li>
+                    Be able to check a nurse’s current capacity and total patient's difficulty
+                    </li>
+                        <br />
+                <a href= "https://github.com/schoi0115/Photo-editor">{"Photo Editor"} </a>
+                    <li>
+                    Able to upload a file from your local computer
+                    </li>
+                    <li>
+                    Play music feature
+                    </li>
+                    <li>
+                    Uploaded photo can be filtered as your selection
+                    </li>
+                    <li>
+                    Non-stored log in and log out feature 
+                    </li>
+                        <br />
                
-                <br />
+                <a href= "https://github.com/schoi0115/login">{"Practice Login"} </a>
+                    <li>
+                    A practice to create a log in function with using cookie and gem 'bcrypt', '3.1.7'
+                    </li>
+                    <li>
+                    Practice of Ruby on rails as a Backend
+                    </li>
+                    <li>
+                    Use React for Frontend
+                    </li>
 
-             
-
-               
-
-                    <a href= "https://github.com/schoi0115/MBTA">{"MBTA-app"} </a>
-          
-                <br />
-              
-           
-                    <a href= "https://github.com/schoi0115/medipill">{"medi pill"} </a>
-           
-                <br />
-         
-     
-                    <a href= "https://github.com/schoi0115/project-n">{"Nurse Tracker application"} </a>
-             
-                <br />
-
-     
+                        <br />
+                <a href= "https://github.com/schoi0115/game_app">{"Game-app"} </a>
+                    <li>
+                    Manually stored backend date + RESTful API request, /GET, /POST, /DELETE, /PATCH
+                    </li>
+                    <li>
+                    Be able to create, add, edit, delete the game card information
+                    </li>
+                        <br />
+                <a href= "https://github.com/schoi0115/MBTA">{"MBTA-app"} </a>
+                    <li>
+                    Vanilla JavaScript application
+                    </li>
+                    <li>
+                    Use metadata API, finding live upcoming training information
+                    </li>
+                    <li>
+                    Rating and comment feature
+                    </li>
+                        <br />
+                <a href= "https://github.com/schoi0115/clock">{"Practice Clock"} </a>
+                    <li>
+                    Pratice to create clock application
+                    </li>
+                    <li>
+                    Be able to create counterdown clock
+                    </li>
+                    <li>
+                    Be able to make D-day 
+                    </li>
+                    <li>
+                    Be able to make normal clock
+                    </li>
+                        <br />
+                <a href= "https://github.com/schoi0115/GaugeChart">{"Gauge Chart Practice"} </a>
+                    <li>
+                   Practice of gauge library
+                    </li>
+                    <li>
+                    https://www.npmjs.com/package/gauge
+                    </li>
+                  
+                        <br />
+                <a href= "https://github.com/schoi0115/Googlemap">{"Google Map with styling"} </a>
+                    <li>
+                    Use GoogleMap API( Maps JavaScript API, Places API, Geocoding API )
+                    </li>
+                    <li>
+                    Be able to search location
+                    </li>
+                    <li>
+                    Custom css map styling
+                    </li>
+                    <li>
+                    Pointing Marker feature
+                    </li>
+                    <li>
+                    Find you current location( find current lat and lng)
+                    </li>
                    
-               
+                    <br />
+                    </p>
+ 
         </div>
 
     );
